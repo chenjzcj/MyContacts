238aaf9ac87e4fbb2545be386d2e03a8a1b57685
felix.zhong <527633405@qq.com>
2018-12-24 10:29:28 +0800

    优化

--- app/src/main/java/huahua/mycontacts/TabAdapter.java
+++ app/src/main/java/huahua/mycontacts/TabAdapter.java
@@ -3,7 +3,6 @@ package huahua.mycontacts;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.ViewGroup;
 
